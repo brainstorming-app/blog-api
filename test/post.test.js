716c94f03feaeb2ffa8af88aca4a686fa791edcd
peta <alexispetalas@gmail.com>
2015-07-13 22:14:18 -0300

    TODO test in pending

--- test/post.test.js
+++ test/post.test.js
@@ -3,9 +3,14 @@ import Supertest from 'supertest'
 
 let request = new Supertest(App)
 
-describe('Post', function() {
-  it('write a post', function(done) {
+describe('Entry Actions', function() {
+  it('POST a Entry', function(done) {
     request
       .get('/').expect("hola vo", done);
   });
+
+  it('GET a Entry');
+  it('GET search many Entrys with limit and offset');
+  it('PUT change Entry');
+
 });
